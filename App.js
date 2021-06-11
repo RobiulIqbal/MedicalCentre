fbccb8c16e73ef6fb21421d422b4b7d8697d463c
RobiulIqbal <qobel98@gmail.com>
2021-06-11 23:46:14 +0700

    First Second Commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 export default function App() {
   return (
     <View>
-      <Text>Hai React native</Text>
+      <Text>Test</Text>
     </View>
   );
 }
